1f16244c36cbb6131cb2750b6236d62157c97346
zeelg <2474008@edu.vaniercollege.qc.ca>
2025-04-26 18:04:16 -0400

    Created comparator classes. Created empty methods for the rest of the classes.

---
+++ src/main/java/org/example/CategoryComparator.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class CategoryComparator implements Comparator<Product> {
+    /**
+     *
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return
+     */
+    @Override
+    public int compare(Product o1, Product o2) {
+        return 0;
+    }
+}
--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.util.Map;
+
 public class Money {
+    private Map<Double, Integer> cashMap;
+
+    /**
+     *
+     * @param amount
+     */
+    public void add(double amount) {}
+
+    /**
+     *
+     * @param amount
+     */
+    public void subtract(double amount) {}
+
+    /**
+     *
+     * @return
+     */
+    public double calculateTotal() {
+        return 0;
+    }
 }
