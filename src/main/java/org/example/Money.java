5c2578ecd0964c2ff46c7f5015d12fe137e82bca
zeelg <2474008@edu.vaniercollege.qc.ca>
2025-04-29 12:10:11 -0400

    Finished writing the Money class. Needed to review it again.

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -15,20 +15,38 @@ public class Money {
      * @param cash a map of the amount and it's kind in cash
      */
     public void add(Map<Double, Integer> cash) {
-
+        for (Map.Entry<Double, Integer> entry : cash.entrySet()) {
+            this.cashMap.put(entry.getKey(), this.cashMap.getOrDefault(entry.getKey(), 0) + entry.getValue());
+        }
     }
 
     /**
      * Removes value from the total money based on the cash to remove
      * @param cash a map of the amount and it's kind in cash
      */
-    public void subtract(Map<Double, Integer> cash) {}
+    public void subtract(Map<Double, Integer> cash) {
+        for (Map.Entry<Double, Integer> entry : cash.entrySet()) {
+            double denomination = entry.getKey();
+            int currentValue = this.cashMap.getOrDefault(denomination, 0);
+            int result = currentValue - entry.getValue();
+
+            if (result > 0) {
+                this.cashMap.put(denomination, result);
+            } else {
+                this.cashMap.remove(denomination);
+            }
+        }
+    }
 
     /**
      * Calculates the total amount
      * @return a map value of amount, and it's kind in cash
      */
-    public Map<Double, Integer> calculateTotal() {
-        return null;
+    public Double calculateTotal() {
+        double total = 0;
+        for (Map.Entry<Double, Integer> entry : cashMap.entrySet()) {
+            total += entry.getKey() * entry.getValue();
+        }
+        return total;
     }
 }
