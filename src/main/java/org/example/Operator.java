890f63ecb955c47c8c4d0ffedd8bb5d52a939d93
zeelg <2474008@edu.vaniercollege.qc.ca>
2025-05-03 15:35:39 -0400

    Added a new field in Operator class + method, to be able to retrieve information in Vending Machine class instead of Buyer.

--- src/main/java/org/example/Operator.java
+++ src/main/java/org/example/Operator.java
@@ -1,22 +1,24 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Operator extends User {
     private String accessLevel;
     private List<String> profitSheets;
+    private Map<Product, Integer> stockingHistory;
 
     public Operator(int id, String name) {
         super(id, name);
         profitSheets = new ArrayList<String>();
         accessLevel = "";
+        stockingHistory = new LinkedHashMap<>();
     }
 
     public Operator(int id, String name, String accessLevel, List<String> profitSheets) {
         super(id, name);
         this.accessLevel = accessLevel;
         this.profitSheets = profitSheets;
+        stockingHistory = new LinkedHashMap<>();
     }
 
     /**
@@ -45,4 +47,49 @@ public class Operator extends User {
      */
     @Override
     public void displayMessage(String message) {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Operator operator = (Operator) o;
+        return Objects.equals(accessLevel, operator.accessLevel) && Objects.equals(profitSheets, operator.profitSheets) && Objects.equals(stockingHistory, operator.stockingHistory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accessLevel, profitSheets, stockingHistory);
+    }
+
+    @Override
+    public String toString() {
+        return "Operator{" + super.toString() +
+                "accessLevel='" + accessLevel + '\'' +
+                ", profitSheets=" + profitSheets +
+                ", stockingHistory=" + stockingHistory +
+                '}';
+    }
+
+    public String getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    public List<String> getProfitSheets() {
+        return profitSheets;
+    }
+
+    public void setProfitSheets(List<String> profitSheets) {
+        this.profitSheets = profitSheets;
+    }
+
+    public Map<Product, Integer> getStockingHistory() {
+        return stockingHistory;
+    }
+
+    public void setStockingHistory(Map<Product, Integer> stockingHistory) {
+        this.stockingHistory = stockingHistory;
+    }
 }
